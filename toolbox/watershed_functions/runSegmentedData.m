23e02eb932bc7521b2bff9b3669af90da459c513
mh105 <mh105@mit.edu>
2022-09-27 19:02:13 -0400

    ADJUST change filtering, saving, and peak indexing

--- toolbox/watershed_functions/runSegmentedData.m
+++ toolbox/watershed_functions/runSegmentedData.m
@@ -1,7 +1,7 @@
 function stats_table = ...
     runSegmentedData(spect, stimes, sfreqs, baseline, seg_time, downsample_spect, ...
-    dur_min, bw_min, ht_db_min, conn_wshed, merge_thresh, max_merges, trim_vol, trim_shift, conn_trim, ...
-    conn_stats, bl_thresh_flag, CI_upper_bl, merge_rule, f_verb, verb_pref, f_disp, f_save, ofile_pref)
+    dur_min, bw_min, conn_wshed, merge_thresh, max_merges, trim_vol, trim_shift, conn_trim, ...
+    conn_stats, bl_thresh_flag, CI_upper_bl, merge_rule, f_verb, verb_pref, f_disp)
 %runSegmentedData: Wrapper that runs:
 %   1. Baseline subtraction
 %   2. Spectrogram segmentation
@@ -12,7 +12,7 @@ function stats_table = ...
 %       [matr_names, matr_fields, peaks_matr, PixelIdxList, PixelList, PixelValues, rgn, bndry, valid_peak_mask] = ...
 %       runSegmentedData(spect, stimes, sfreqs, baseline, seg_time, downsample_spect, ...
 %       dur_min, bw_min, conn_wshed, merge_thresh, max_merges, trim_vol, trim_shift, conn_trim, ...
-%       conn_stats, bl_thresh_flag, CI_upper_bl, merge_rule, f_verb, verb_pref, f_disp, f_save, ofile_pref)
+%       conn_stats, bl_thresh_flag, CI_upper_bl, merge_rule, f_verb, verb_pref, f_disp)
 %
 % INPUTS:
 %   spect        --  2D image data used to extract TFpeaks [freq, time] --required
@@ -26,7 +26,6 @@ function stats_table = ...
 %   downsample_spect   --  2x1 double indicating number of rows and columns to downsize spect to.
 %   dur_min      -- minimum duration allowed
 %   bw_min       -- minimum bandwidth allowed
-%   ht_db_min    -- minimum height in dB allowed
 %   conn_wshed   -- pixel connection to be used by peaksWShed. default 8.
 %   merge_thresh -- threshold weight value for when to stop merge rule. default 8.
 %   max_merges   -- maximum number of merges to perform. default inf.
@@ -52,10 +51,6 @@ function stats_table = ...
 %   verb_pref    -- prefix string for verbose output. defaults to ''.
 %   f_disp       -- flag indicator of whether to plot.
 %                   defaults to false, unless using default data.
-%   f_save       -- integer indicator of whether to save output files and how much
-%                   information to save. [0 = no saving, 1 = save fewer peak stats,
-%                   2 = save all peak stats]. Default 0.
-%   ofile_pref   -- string of path and data name for outputs. default './'.
 %
 % OUTPUTS:
 %   stats_table     -- table of peak statistics
@@ -96,68 +91,56 @@ if nargin < 8 || isempty(bw_min)
     bw_min = 0;
 end
 
-if nargin < 9 || isempty(ht_db_min)
-    ht_db_min = 7.63;
-end
-
-if nargin < 10 || isempty(conn_wshed)
+if nargin < 9 || isempty(conn_wshed)
     conn_wshed = 8;
 end
 
-if nargin < 11 || isempty(merge_thresh)
+if nargin < 10 || isempty(merge_thresh)
     merge_thresh = 8;
 end
 
-if nargin < 12 || isempty(max_merges)
+if nargin < 11 || isempty(max_merges)
     max_merges = inf;
 end
 
-if nargin < 13 || isempty(trim_vol)
+if nargin < 12 || isempty(trim_vol)
     trim_vol = 0.8;
 end
 
-if nargin < 14 || isempty(trim_shift)
+if nargin < 13 || isempty(trim_shift)
     trim_shift = [];
 end
 
-if nargin < 15 || isempty(conn_trim)
+if nargin < 14 || isempty(conn_trim)
     conn_trim = 8;
 end
 
-if nargin < 16 || isempty(conn_stats)
+if nargin < 15 || isempty(conn_stats)
     conn_stats = 8;
 end
 
-if nargin < 17 || isempty(bl_thresh_flag)
+if nargin < 16 || isempty(bl_thresh_flag)
     bl_thresh_flag = false;
 end
 
-if nargin < 18 || isempty(CI_upper_bl)
+if nargin < 17 || isempty(CI_upper_bl)
     CI_upper_bl = [];
 end
 
-if nargin < 19 || isempty(merge_rule)
+if nargin < 18 || isempty(merge_rule)
     merge_rule = 'default';
 end
 
-if nargin < 20 || isempty(f_verb)
+if nargin < 19 || isempty(f_verb)
     f_verb = 1;
 end
 
-if nargin < 21 || isempty(verb_pref)
+if nargin < 20 || isempty(verb_pref)
     verb_pref = '';
 end
 
-if nargin < 22 || isempty(f_disp)
+if nargin < 21 || isempty(f_disp)
     f_disp = 0;
-end
-
-if nargin < 23 || isempty(f_save)
-    f_save = 0;
-end
-
-if nargin < 24 || isempty(ofile_pref)
-    ofile_pref = './';
 end
 
 %******************
@@ -174,56 +157,47 @@ if isempty(trim_shift)
     trim_shift = min(spect,[],'all');
 end
 
-
 %% Segment spectrogram data
 [data_segs, x_segs] = segmentData(spect, stimes, sfreqs, seg_time, f_verb, verb_pref);
 
-
 %% Extract TFpeaks from spectrogram segments
-
 % Initialize storage for parallel processing of image segs
 n_segs = length(data_segs);
 stats_tables = cell(n_segs,1);
 
 % In parallel, find TFpeaks for each seg
 computetime = tic;
-if n_segs > 1
 
-    % Check for parallel processing toolbox and set up loading bar
-    v = ver;
-    haspar = any(strcmp({v.Name}, 'Parallel Computing Toolbox'));
+% Check for parallel processing toolbox and set up loading bar
+v = ver;
+haspar = any(strcmp({v.Name}, 'Parallel Computing Toolbox'));
+if haspar
+    D = parallel.pool.DataQueue;
+    h = waitbar(0, 'Processing Segments...');
+    afterEach(D, @nUpdateWaitbar);
+else
+    h = waitbar(0, 'Processing Segments...');
+end
+segments_processed = 1;
+
+if f_verb > 0
+    disp([verb_pref 'Processing segments...']);
+end
+
+%MAIN LOOP ACROSS SEGMENTS
+parfor ii = 1:n_segs
+    if length(x_segs{ii})>1
+        stats_tables{ii} =  extractTFPeaks(data_segs{ii},x_segs{ii},sfreqs,ii,conn_wshed,merge_thresh,max_merges,downsample_spect,dur_min,bw_min,trim_vol,trim_shift,conn_trim,conn_stats,bl_threshold,merge_rule,f_verb-1,['  ' verb_pref],f_disp);
+    end
+    
+    % Update loading bar
     if haspar
-        D = parallel.pool.DataQueue;
-        h = waitbar(0, 'Processing Segments...');
-        afterEach(D, @nUpdateWaitbar);
+        send(D, ii);
     else
-        h = waitbar(0, 'Processing Segments...');
-    end
-    segments_processed = 1;
-
-    if f_verb > 0
-        disp([verb_pref 'Processing segments...']);
-    end
-
-    %MAIN LOOP ACROSS SEGMENTS
-    parfor ii = 1:n_segs
-        if length(x_segs{ii})>1
-            stats_tables{ii} =  extractTFPeaks(data_segs{ii},x_segs{ii},sfreqs,ii,conn_wshed,merge_thresh,max_merges,downsample_spect,dur_min,bw_min,trim_vol,trim_shift,conn_trim,conn_stats,bl_threshold,merge_rule,f_verb-1,['  ' verb_pref],f_disp);
-        end
-
-        % Update loading bar
-        if haspar
-            send(D, ii);
-        else
-            h = waitbar(ii/n_segs,  [num2str(ii) ' out of ' num2str(n_segs) ' (' num2str((ii/n_segs*100),'%.2f') '%) segments processed...']);
-        end
-    end
-    delete(h); % delete loading bar
-else % if there is only 1 segment total
-    for ii = 1:n_segs
-        stats_tables{ii} = extractTFPeaks(data_segs{ii},x_segs{ii},sfreqs,ii,conn_wshed,merge_thresh,max_merges,trim_vol,trim_shift,conn_trim,conn_stats,bl_thresh_flag,merge_rule,f_verb-1,['  ' verb_pref],f_disp);
+        h = waitbar(ii/n_segs,  [num2str(ii) ' out of ' num2str(n_segs) ' (' num2str((ii/n_segs*100),'%.2f') '%) segments processed...']);
     end
 end
+delete(h); % delete loading bar
 
 %Add a parallel friendly waitbar
     function nUpdateWaitbar(~)
@@ -235,21 +209,8 @@ if f_verb > 0
     disp([verb_pref '  Computing took ' num2str(toc(computetime)/60) ' minutes.']);
 end
 
-%% Assembles peaks stats for all segs into single table and filters
+%% Assembles peaks stats for all segs into single table
 stats_table = cat(1,stats_tables{:});
-
-%Get filter index
-filter_idx = filter_peak_statstable(stats_table, [dur_min 5], [bw_min 15], [-inf inf], [ht_db_min inf]);
-%Filter the table
-stats_table = stats_table(filter_idx,:);
-
-%% Save data
-if f_save
-    if f_verb > 0
-        disp(['Saving ' ofile_pref '...']);
-    end
-    save(ofile_pref,"stats_table");
-end
 
 end
 
