d9378d1d33e0a03a3edcc28453e357459aab35ab
mh105 <mh105@mit.edu>
2023-01-31 21:43:34 -0500

    ADD feature selection in example_script

--- toolbox/TFpeak_functions/computePeakStatsTable.m
+++ toolbox/TFpeak_functions/computePeakStatsTable.m
@@ -1,4 +1,4 @@
-function stats_table = computePeakStatsTable(regions,boundaries,data,xvalues,yvalues,segment_num, properties)
+function stats_table = computePeakStatsTable(regions,boundaries,data,xvalues,yvalues,segment_num, features)
 % COMPUTEPEAKSTATSTABLE Creates a table of the region properties for the peaks
 %
 % USAGE:
@@ -11,7 +11,7 @@ function stats_table = computePeakStatsTable(regions,boundaries,data,xvalues,yva
 %   xvalues      -- x axis of image data. default 1:size(data,2).
 %   yvalues      -- y axis of image data. default 1:size(data,1).
 %   segment_num  -- segment number if data comes from larger image. default 1.
-%   properties -- cell array of properties to include, can be any subset of
+%   features   -- cell array of features to include, can be any subset of
 %                   {'Area', 'Bandwidth', 'Boundaries', 'BoundingBox', 'Duration', 'Height', 'HeightData', 
 %                     'PeakFrequency', 'PeakTime', 'SegmentNum', 'Volume'}
 %                   or 'all', (default: 'all')
@@ -50,8 +50,8 @@ if nargin<6 || isempty(segment_num)
     segment_num = 1;
 end
 
-if nargin<7 || isempty(properties) || any(strcmpi(properties,'all'))
-    properties = {'Area', 'Bandwidth', 'Boundaries', 'BoundingBox', 'Duration', 'Height', 'HeightData', 'PeakFrequency', 'PeakTime', 'SegmentNum', 'Volume'};
+if nargin<7 || isempty(features) || any(strcmpi(features,'all'))
+    features = {'Area', 'Bandwidth', 'Boundaries', 'BoundingBox', 'Duration', 'Height', 'HeightData', 'PeakFrequency', 'PeakTime', 'SegmentNum', 'Volume'};
 end
 
 assert(iscell(regions) && ~isempty(regions),'Regions must be a cell array');
@@ -66,19 +66,19 @@ end
 
 %Compute the stats table
 r_props = {};
-if any(strcmpi(properties,'Area'))
+if any(strcmpi(features,'Area'))
     r_props = cat(2,r_props,'Area');
 end
 
-if any(strcmpi(properties,'BoundingBox')) || any(strcmpi(properties,'Bandwidth')) || any(strcmpi(properties,'Duration'))
+if any(strcmpi(features,'BoundingBox')) || any(strcmpi(features,'Bandwidth')) || any(strcmpi(features,'Duration'))
     r_props = cat(2,r_props,'BoundingBox');
 end
 
-if any(strcmpi(properties,'PeakTime')) || any(strcmpi(properties,'PeakFrequency')) 
+if any(strcmpi(features,'PeakTime')) || any(strcmpi(features,'PeakFrequency')) 
     r_props = cat(2,r_props,'WeightedCentroid');
 end
 
-if any(strcmpi(properties,'Height')) || any(strcmpi(properties,'HeightData')) || any(strcmpi(properties,'Volume')) 
+if any(strcmpi(features,'Height')) || any(strcmpi(features,'HeightData')) || any(strcmpi(features,'Volume')) 
     r_props = cat(2,r_props,'PixelValues');
 end
 
@@ -93,17 +93,17 @@ seg_startx = xvalues(1);
 seg_starty = yvalues(1);
 
 %Remove dead rows
-if any(strcmpi(properties,'Area'))
+if any(strcmpi(features,'Area'))
     good_indices = stats_table.Area > 0;
 end
 
 stats_table = stats_table(good_indices,:);
 boundaries = boundaries(good_indices);
-regions = regions(good_indices);
+% regions = regions(good_indices);
 
 %% Compute statistics
 %Bounding Box
-if any(strcmpi(properties,'BoundingBox')) || any(strcmpi(properties,'Duration')) || any(strcmpi(properties,'Bandwidth'))
+if any(strcmpi(features,'BoundingBox')) || any(strcmpi(features,'Duration')) || any(strcmpi(features,'Bandwidth'))
     stats_table.BoundingBox(:,1) = stats_table.BoundingBox(:,3)*dx + seg_startx;
     stats_table.BoundingBox(:,2) = stats_table.BoundingBox(:,4)*dy + seg_starty;
     stats_table.BoundingBox(:,3) = stats_table.BoundingBox(:,3)*dx;
@@ -113,21 +113,21 @@ if any(strcmpi(properties,'BoundingBox')) || any(strcmpi(properties,'Duration'))
 end
 
 %Area
-if any(strcmpi(properties,'Area'))
+if any(strcmpi(features,'Area'))
     stats_table.Area = stats_table.Area*dx*dy;
     stats_table.Properties.VariableDescriptions{'Area'} = 'Time-frequency area of peak';
     stats_table.Properties.VariableUnits{'Area'} = 'sec*Hz';
 end
 
 %Volume
-if any(strcmpi(properties,'Volume'))
+if any(strcmpi(features,'Volume'))
     stats_table.Volume = cellfun(@(x)sum(x)*dx*dy, stats_table.PixelValues);
     stats_table.Properties.VariableDescriptions{'Volume'} = 'Time-frequency volume of peak in s*μV^2';
     stats_table.Properties.VariableUnits{'Volume'} = 'sec*μV^2';
 end
 
 %Boundaries
-if any(strcmpi(properties,'Boundaries'))
+if any(strcmpi(features,'Boundaries'))
     [a,b] = cellfun(@(x)ind2sub(size(data),x),boundaries,'UniformOutput',false);
     stats_table.Boundaries = cellfun(@(a,b)[(b-1)*dx+seg_startx, (a-1)*dy+seg_starty], a, b, 'Uniform', 0); % a,b in pixel indices
     stats_table.Properties.VariableDescriptions{'Boundaries'} = '(time, frequency) of peak region boundary pixels';
@@ -135,49 +135,49 @@ if any(strcmpi(properties,'Boundaries'))
 end
 
 %Peak Time
-if any(strcmpi(properties,'PeakTime'))
+if any(strcmpi(features,'PeakTime'))
     stats_table.PeakTime = stats_table.WeightedCentroid(:,1)*dx+seg_startx; % WeightedCentroid in spatial coordinates
     stats_table.Properties.VariableDescriptions{'PeakTime'} = 'Peak time based on weighted centroid';
     stats_table.Properties.VariableUnits{'PeakTime'} = 'sec';
 end
 
 %Peak Frequency
-if any(strcmpi(properties,'PeakFrequency'))
+if any(strcmpi(features,'PeakFrequency'))
     stats_table.PeakFrequency = stats_table.WeightedCentroid(:,2)*dy+seg_starty; % WeightedCentroid in spatial coordinates
     stats_table.Properties.VariableDescriptions{'PeakFrequency'} = 'Peak frequency based on weighted centroid';
     stats_table.Properties.VariableUnits{'PeakFrequency'} = 'Hz';
 end
 
 %Height
-if any(strcmpi(properties,'Height'))
+if any(strcmpi(features,'Height'))
     stats_table.Height = cellfun(@max,stats_table.PixelValues) - cellfun(@min,stats_table.PixelValues);
     stats_table.Properties.VariableDescriptions{'Height'} = 'Peak height above baseline';
     stats_table.Properties.VariableUnits{'Height'} = 'μV^2/Hz';
 end
 
 %Duration
-if any(strcmpi(properties,'Duration'))
+if any(strcmpi(features,'Duration'))
     stats_table.Duration = stats_table.BoundingBox(:,3); % BoundingBox in spatial coordinates
     stats_table.Properties.VariableDescriptions{'Duration'} = 'Peak duration in seconds';
     stats_table.Properties.VariableUnits{'Duration'} = 'sec';
 end
 
 %Bandwidth
-if any(strcmpi(properties,'Bandwidth'))
+if any(strcmpi(features,'Bandwidth'))
     stats_table.Bandwidth = stats_table.BoundingBox(:,4); % BoundingBox in spatial coordinates
     stats_table.Properties.VariableDescriptions{'Bandwidth'} = 'Peak bandwidth in Hz';
     stats_table.Properties.VariableUnits{'Bandwidth'} = 'Hz';
 end
 
 %Segment Number
-if any(strcmpi(properties,'SegmentNum'))
+if any(strcmpi(features,'SegmentNum'))
     stats_table.SegmentNum(:,1) = segment_num;
     stats_table.Properties.VariableDescriptions{'SegmentNum'} = 'Data segment number';
     stats_table.Properties.VariableUnits{'SegmentNum'} = '#';
 end
 
 %Region data
-if any(strcmpi(properties,'HeightData'))
+if any(strcmpi(features,'HeightData'))
     stats_table.Properties.VariableNames{'PixelValues'} = 'HeightData';
     stats_table.Properties.VariableDescriptions{'HeightData'} = 'Height of all pixels within peak region';
     stats_table.Properties.VariableUnits{'HeightData'} = 'μV^2/Hz';
@@ -186,7 +186,7 @@ else
 end
 
 %Remove bounding box if not used
-if ~any(strcmpi(properties,'BoundingBox'))
+if ~any(strcmpi(features,'BoundingBox'))
     stats_table.BoundingBox = [];
 end
 
