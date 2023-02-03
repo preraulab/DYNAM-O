75a109fdcf77906e58cc5f3a2d9359ef21c2c91a
Thomas Possidente <tpossidente@bwh.harvard.edu>
2023-02-03 11:49:34 -0500

    MINOR added compute_rate and norm_dim to verbose display

--- toolbox/SOpowphase_functions/TFPeakHistogram.m
+++ toolbox/SOpowphase_functions/TFPeakHistogram.m
@@ -69,7 +69,7 @@ end
 num_Cbins = length(C_cbins);
 
 % Display the CPH settings
-display_soph_setting(verbose, Cmetric_label, C_range, C_binsizestep, freq_range, freq_binsizestep, norm_method, min_time_in_bin)
+display_soph_setting(verbose, Cmetric_label, C_range, C_binsizestep, freq_range, freq_binsizestep, norm_method, min_time_in_bin, compute_rate, norm_dim)
 
 %% Create the histogram
 % Intialize Cmetric * freq matrix
@@ -176,7 +176,7 @@ if plot_on
     ylabel('Frequency (Hz)');
 end
 
-function display_soph_setting(verbose, Cmetric_label, C_range, C_binsizestep, freq_range, freq_binsizestep, norm_method, min_time_in_bin)
+function display_soph_setting(verbose, Cmetric_label, C_range, C_binsizestep, freq_range, freq_binsizestep, norm_method, min_time_in_bin, compute_rate, norm_dim)
 % Display CPH settings
 if ischar(verbose)
     disp(verbose)
@@ -186,7 +186,8 @@ elseif verbose
         '    Frequency Window Size: ' num2str(freq_binsizestep(1)) ' Hz, Window Step: ' num2str(freq_binsizestep(2)) ' Hz', newline,...
         '    Frequency Range: ', num2str(freq_range(1)) '-' num2str(freq_range(2)) ' Hz', newline,...
         '    ', Cmetric_label, ' Window Size: ' num2str(C_binsizestep(1)) ', Window Step: ' num2str(C_binsizestep(2)), newline,...
-        '    ', Cmetric_label, ' Range: ', num2str(C_range(1)), '-', num2str(C_range(2)),  newline];
+        '    ', Cmetric_label, ' Range: ', num2str(C_range(1)), '-', num2str(C_range(2)),  newline...
+        '    ', 'Compute Rate: ', compute_rate, ', norm_dim: ', norm_dim];
 
     if ~isempty(norm_method)
         display_message = [display_message, '    Normalization Method: ', num2str(norm_method), newline];
