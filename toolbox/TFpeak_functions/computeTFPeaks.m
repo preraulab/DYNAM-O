8ae9bf44f0517d613c84909cb215bee9b27f2d35
mh105 <mh105@mit.edu>
2023-11-01 15:04:57 -0400

    Fix dur_min in double watershed

--- toolbox/TFpeak_functions/computeTFPeaks.m
+++ toolbox/TFpeak_functions/computeTFPeaks.m
@@ -165,7 +165,7 @@ if double_watershed
     % Compute multitaper spectrogram using new parameters with smaller spectral resolution
     [spect, stimes, sfreqs,...
         downsample_spect, seg_time, merge_thresh,...
-        dur_min, bw_min, dur_max, bw_max, ht_db_min] = compute_spectrogram([2,3], [2,0.05], data_trunc, Fs, quality_setting, verbose);
+        ~, bw_min, dur_max, bw_max, ht_db_min] = compute_spectrogram([2,3], [2,0.05], data_trunc, Fs, quality_setting, verbose);
     stimes = stimes + t_data_trunc(1); % adjust the time axis to t_data
     
     % Update artifact vector
