905185836e1996ff0fc4751a0d7c4358f51e733d
SoSaremsky <sophiesaremsky@gmail.com>
2023-11-20 13:00:11 -0500

    Added params for taper/window params and hanning window

--- toolbox/TFpeak_functions/computeTFPeaks.m
+++ toolbox/TFpeak_functions/computeTFPeaks.m
@@ -38,6 +38,7 @@ function [stats_table, spect, stimes, sfreqs, data_trunc, t_data_trunc, artifact
 %       time_window_params (opt):  [1x2] or [2x2] double - time window parameters used to compute the spectrogram in 
 %                                       single or double watershed (1x2 if single, 2x2 if double). Default = [[1,0.05];[2,0.05]]
 %       double_watershed_hanning (opt): logical - whether to use hanning window for the second round of watershed. Default = false
+%       trim_vol (opt):             double - fraction maximum trimmed volume (from 0 to 1), i.e. 1 means no trim. Default = 0.8
 %
 %   Outputs:
 %       stats_table:  table - time, frequency, height, SOpower, and SOphase
@@ -81,6 +82,7 @@ addOptional(p, 'quality_setting', 'fast', @(x) validateattributes(x,{'char','num
 addOptional(p, 'taper_params', [], @(x) validateattributes(x,{'numeric'},{'real','nonempty','nonnan'}));
 addOptional(p, 'time_window_params', [], @(x) validateattributes(x,{'numeric'},{'real','nonempty','nonnan'}));
 addOptional(p, 'double_watershed_hanning', false, @(x) validateattributes(x,{'logical'},{'real','nonempty', 'nonnan'}));
+addOptional(p, 'trim_vol', 0.8, @(x) validateattributes(x,{'numeric'},{'real','finite','nonnan'}));
 
 parse(p,varargin{:});
 parser_results = struct2cell(p.Results); %#ok<NASGU>
@@ -194,19 +196,18 @@ if double_watershed
             downsample_spect, seg_time, merge_thresh,...
             ~, bw_min, dur_max, bw_max, ht_db_min] = compute_spectrogram(taper_params(2,:), time_window_params(2,:), data_trunc, Fs, quality_setting, verbose);
         stimes = stimes + t_data_trunc(1); % adjust the time axis to t_data
-    % HANNING WINDOW
-    else
-        dsfreqs = 0.1; % For consistency with our results we expect a df of 0.1 Hz or less
-        freq_range = [0,30]; % frequency range to compute spectrum over (Hz)
-        nfft = 2^(nextpow2(Fs/dsfreqs)); % zero pad data to this minimum value for fft
-        detrend = 'constant'; % do not detrend
-        weight = 'unity'; % each taper is weighted the same
-        ploton = false; % do not plot out
-        mts_verbose = false; % suppress verbose messages
     
-        taper_params = [1,1];
-        time_window_params = [4,.05];
-        [spect,stimes,sfreqs] = hanning_spectrogram(data_trunc, Fs, freq_range, taper_params, time_window_params, nfft, detrend, weight, ploton, mts_verbose);
+    else
+        
+        taper_params_h = [1,1];
+        time_window_params_h = [4,0.05];
+        
+        bw_min = 4/time_window_params_h(1)/2;
+        
+        [spect, stimes, sfreqs,...
+            downsample_spect, seg_time, merge_thresh,...
+            ~, ~, dur_max, bw_max, ht_db_min] = compute_spectrogram_hanning(taper_params_h, time_window_params_h, data_trunc, Fs, quality_setting, verbose);
+        stimes = stimes + t_data_trunc(1); 
 
     end
     
@@ -234,7 +235,7 @@ if double_watershed
     compute_features = unique([features, {'Duration', 'Bandwidth', 'PeakFrequency', 'Height'}]);
     if any(strcmpi(features, 'PeakStage')); compute_features = unique([compute_features, 'PeakTime']); end
     
-    stats_table = runSegmentedData(spect_masked, stimes, sfreqs, baseline, seg_time, downsample_spect, compute_features, dur_min, bw_min, [], merge_thresh, [], 0.99);
+    stats_table = runSegmentedData(spect_masked, stimes, sfreqs, baseline, seg_time, downsample_spect, compute_features, dur_min, bw_min, [], merge_thresh, [], trim_vol);
     
     if verbose
         disp(['[2nd] TF-peak extraction took ' datestr(seconds(toc(tfp)),'HH:MM:SS'), newline]);
@@ -338,3 +339,4 @@ else
 end
 
 end
+
