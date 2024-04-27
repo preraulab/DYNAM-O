1e049f12b145d0f7cbf3f5ea77fbbc687060da53
Michael Prerau, Ph.D. <78376124+prerau@users.noreply.github.com>
2024-04-27 09:10:02 -0400

    Refinement and use of structures
    Updated functions to remove unnecessary or experimental inputs. Got rid of redundant functionality. Updated example script to work properly.

--- toolbox/TFpeak_functions/option_sets/detection_opts.m
+++ toolbox/TFpeak_functions/option_sets/detection_opts.m
@@ -12,35 +12,34 @@ addOptional(p, 'verbose', true, @(x) validateattributes(x,{'logical'},{'real','n
 addOptional(p, 'double_watershed', true, @(x) validateattributes(x,{'logical'},{'real','nonempty', 'nonnan'}));
 %Frequency refinment using 1Hz df hann spectrum
 addOptional(p, 'refinement', true, @(x) validateattributes(x,{'logical'},{'real','nonempty', 'nonnan'}));
-%Remove descritization by interpolating to a much higher df during the refinement stage
-%addOptional(p, 'refine_method', 'spline_interp', @(x) validatestring(x,{'spline_interp','spline_opt','spect_max'}));
 
 %Frequency bin resolution of the spectrogram
 addOptional(p, 'dsfreqs', 0.1, @(x) validateattributes(x,{'scalar','numeric'},{'real','nonempty', 'nonnan'}));
+%Fixed quality setting by name 'fast', 'precision', 'paper', 
+addOptional(p, 'quality_setting', '', @(x) validateattributes(x,{'char','numeric'},{}));
 %Decimation steps for the spectrogram prior to watershed
 addOptional(p, 'downsample_spect', [2 2], @(x) validateattributes(x,{'vector','numeric'},{}));
 %Segment size for spectrogram paralleization
 addOptional(p, 'seg_time', 30, @(x) validateattributes(x,{'scalar','numeric'},{}));
-%Fixed quality setting by name 'fast', 'precision', 'paper'
-addOptional(p, 'quality_setting', 'fast', @(x) validateattributes(x,{'char','numeric'},{}));
 %Remove peaks at the edge of the bounds in masking step
 addOptional(p, 'remove_edge_peaks', true, @(x) validateattributes(x,{'logical'},{'real','nonempty', 'nonnan'}));
 
 %Watershed parameters
 %Threshold weight value for when to stop merge rule
-%addOptional(p, 'merge_thresh', 11, @(x) validateattributes(x,{'scalar','numeric'},{}));
+addOptional(p, 'merge_thresh', 11, @(x) validateattributes(x,{'scalar','numeric'},{}));
 %Maximum number of merges to perform
-%addOptional(p, 'max_merges', inf, @(x) validateattributes(x,{'scalar','numeric'},{}));
+addOptional(p, 'max_merges', inf, @(x) validateattributes(x,{'scalar','numeric'},{}));
 %Fraction maximum trimmed volume
-%addOptional(p, 'trim_vol', 0.8, @(x) validateattributes(x,{'scalar','numeric'},{}));
-%Pixel connection used in trimming
-%addOptional(p, 'conn_trim', 8, @(x) validateattributes(x,{'scalar','numeric'},{}));
-%Pixel connection used by watershed
-%addOptional(p, 'conn_wshed', 8, @(x) validateattributes(x,{'scalar','numeric'},{}));
-%Min duration allowed
-%addOptional(p, 'dur_min', 0, @(x) validateattributes(x,{'scalar','numeric'},{}));
-%Min bandwidth allowed
-%addOptional(p, 'bw_min', 0, @(x) validateattributes(x,{'scalar','numeric'},{}));
+addOptional(p, 'trim_vol', 0.8, @(x) validateattributes(x,{'scalar','numeric'},{}));
+%Max duration allowed
+addOptional(p, 'dur_max', 5, @(x) validateattributes(x,{'scalar','numeric'},{}));
+%Max bandwidth allowed
+addOptional(p, 'bw_max', 15, @(x) validateattributes(x,{'scalar','numeric'},{}));
+
+%Features to compute
+all_features = {'all', 'Area', 'Bandwidth', 'Boundaries', 'BoundingBox', 'Duration', 'Height',  'HeightData',...
+'PeakFrequency', 'PeakTime', 'SegmentNum', 'Volume', 'PeakStage'};
+addOptional(p,'features','all',@(x)all(ismember(x,all_features)))
 
 parse(p,varargin{:});
 opts = p.Results; 
