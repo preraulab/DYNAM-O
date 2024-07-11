4cdd586e7fdc229cc51f5e0655b08775177e7d3e
Michael Prerau, Ph.D. <78376124+prerau@users.noreply.github.com>
2024-07-11 10:53:40 -0400

    Update runDYNAMO.m

--- runDYNAMO.m
+++ runDYNAMO.m
@@ -25,8 +25,6 @@
 %       runDYNAMO();
 %
 %   Copyright 2024 Prerau Lab - http://www.sleepEEG.org
-%   This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
-%   (http://creativecommons.org/licenses/by-nc-sa/4.0/)
 %
 %   Please provide the following citation for all use:
 %       Patrick A Stokes, Preetish Rath, Thomas Possidente, Mingjian He, Shaun Purcell, Dara S Manoach,
