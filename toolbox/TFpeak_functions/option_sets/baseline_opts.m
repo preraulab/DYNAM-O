88ed9003ab5f78ec598b2d97020c2d4b7dc60389
mh105 <alexhe0218@gmail.com>
2024-12-04 21:25:34 -0800

    ENH: fix input parameters to computeTFPeaks

--- toolbox/TFpeak_functions/option_sets/baseline_opts.m
+++ toolbox/TFpeak_functions/option_sets/baseline_opts.m
@@ -5,13 +5,14 @@ p = inputParser;
 %*******************************************
 % Generate Baseline Options Structure
 %*******************************************
-addOptional(p, 'baseline_exclude',logical([]),@(x) validateattributes(x,{'logical'},{'real','finite','nonnan'}));
-%Sleep stages to include for baseline detection
+%Sleep stages to include for baseline computation
 addOptional(p, 'baseline_stages',[1,2,3,4,5],@(x) validateattributes(x, {'numeric', 'vector'}, {'real', 'nonempty'}));
-%Percentile to use for fixed baseline detection
+%Logical indices for time points to exclude for baseline computation
+addOptional(p, 'baseline_exclude',logical([]),@(x) validateattributes(x,{'logical'},{'real','finite','nonnan'}));
+%Percentile to use for fixed baseline computation
 addOptional(p, 'baseline_ptile',2,@(x) validateattributes(x, {'numeric', 'scalar'}, {'real', 'nonempty'}));
 %Start and stop times for baseline trimming OR integer representing buffer time (min) around the first and last sleep period.
 addOptional(p, 'baseline_trim',[-inf inf],@(x) validateattributes(x, {'numeric', 'vector'}, {'real'}));
 
 parse(p,varargin{:});
-opts = p.Results; 
+opts = p.Results;
--- toolbox/TFpeak_functions/option_sets/detection_opts.m
+++ toolbox/TFpeak_functions/option_sets/detection_opts.m
@@ -11,18 +11,39 @@ addOptional(p, 'verbose', true, @(x) validateattributes(x,{'logical'},{'real','n
 %Double vs single watershed
 addOptional(p, 'double_watershed', true, @(x) validateattributes(x,{'logical'},{'real','nonempty', 'nonnan'}));
 
+%Multitaper method parameters
 %Frequency bin resolution of the spectrogram
 addOptional(p, 'dsfreqs', 0.1, @(x) validateattributes(x,{'scalar','numeric'},{'real','nonempty', 'nonnan'}));
+%Taper parameters [time half-bandwidth product, number of tapers]
+addOptional(p, 'mtm_taper_params', [2, 3], @(x) validateattributes(x,{'vector'},{'real','nonempty', 'nonnan'}));
+%Window length in spectrogram computation for the first round of watershed
+addOptional(p, 'mtm_window_length_1', 1, @(x) validateattributes(x,{'scalar','numeric'},{'real','nonempty', 'nonnan'}));
+%Window length in spectrogram computation for the second round of watershed
+addOptional(p, 'mtm_window_length_2', 2, @(x) validateattributes(x,{'scalar','numeric'},{'real','nonempty', 'nonnan'}));
+%Window step size in spectrogram computations
+addOptional(p, 'mtm_window_stepsize', 0.05, @(x) validateattributes(x,{'scalar','numeric'},{'real','nonempty', 'nonnan'}));
 
 %Watershed parameters
 %Decimation steps for the spectrogram prior to watershed: first index along the time axis; second index along frequency
-addOptional(p, 'downsample_spect', [2 2], @(x) validateattributes(x,{'vector','numeric'},{}));
+addOptional(p, 'downsample_spect', [], @(x) validateattributes(x,{'vector','numeric'},{})); % set by quality_setting
 %Segment size for spectrogram paralleization
-addOptional(p, 'seg_time', 30, @(x) validateattributes(x,{'scalar','numeric'},{}));
+addOptional(p, 'seg_time', [], @(x) validateattributes(x,{'scalar','numeric'},{})); % set by quality_setting
 %Threshold weight value for when to stop merge rule
-addOptional(p, 'merge_thresh', 11, @(x) validateattributes(x,{'scalar','numeric'},{}));
-%Fixed quality setting by name 'fast', 'precision', 'paper', 
-addOptional(p, 'quality_setting', '', @(x) validateattributes(x,{'char','numeric'},{}));
+addOptional(p, 'merge_thresh', [], @(x) validateattributes(x,{'scalar','numeric'},{})); % set by quality_setting
+%Fixed quality setting by string options: 'stokes_2023', 'precision', or 'default'
+% 'stokes_2023':
+%   downsample_spect = [];
+%   seg_time = 60; (seconds)
+%   merge_thresh = 8; (merge weight unit)
+% 'precision':
+%   downsample_spect = [];
+%   seg_time = 30; (seconds)
+%   merge_thresh = 8; (merge weight unit)
+% 'default':
+%   downsample_spect = [2, 2]; (steps, steps)
+%   seg_time = 30; (seconds)
+%   merge_thresh = 11; (merge weight unit)
+addOptional(p, 'quality_setting', 'default', @(x) validateattributes(x,{'char','numeric'},{}));
 
 %Merging and trimming parameters
 %Maximum number of merges to perform
@@ -39,8 +60,8 @@ addOptional(p, 'refinement', true, @(x) validateattributes(x,{'logical'},{'real'
 
 %Features to compute
 all_features = {'all', 'Area', 'Bandwidth', 'Boundaries', 'BoundingBox', 'Duration', 'Height',  'HeightData',...
-'PeakFrequency', 'PeakTime', 'SegmentNum', 'Volume', 'PeakStage'};
+    'PeakFrequency', 'PeakTime', 'SegmentNum', 'Volume', 'PeakStage'};
 addOptional(p,'features','all',@(x)all(ismember(x,all_features)))
 
 parse(p,varargin{:});
-opts = p.Results; 
+opts = p.Results;
