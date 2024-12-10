0858f942fbc04c2921595a0837a9acbbe935de82
mh105 <alexhe0218@gmail.com>
2024-12-09 22:26:19 -0800

    SYS: remove unused functions for filesystem cleaning

--- toolbox/TFpeak_functions/Contents.m
+++ toolbox/TFpeak_functions/Contents.m
@@ -4,12 +4,15 @@
 %   cell2Ldata            - takes rgn matrix and converts to labeled 2D form
 %   computeMergeWeights   - determines the weights of directed adjacencies between regions
 %   computePeakStatsTable - creates a table of the region properties for the peaks
+%
 %   computeTFPeaks        - [Main function] run watershed pipeline to extract time-frequency peaks
-%   extractTFPeaks        - determines the peak regions within a spectral topography and extracts a set of features for each
+%
+%   extractTFPeaks        - determines the peak regions within a spectral topography and extracts a set of features
 %   filterStatsTable      - gets indices of peaks that pass the BW, duration,
 %   Ldata2graph           - label region border pixels and determine region adjacencies.
 %   mergeRegions          - updates the pixel lists in rgn and borders and the adjacencies in 
 %   mergeWshedSegment     - takes the labeled image, borders, and adjacencies output
+%   refineTFpeaks         - refines the peak frequency estimate using a Hann spectrogram with 1Hz spectral resolution
 %   removeBaseline        - subtract percentile baseline from spectrogram 
 %   runSegmentedData      - wrapper that runs 1) baseline subtraction, 2) spectrogram
 %   runWatershed          - determines peak regions using matlab watershed function.
