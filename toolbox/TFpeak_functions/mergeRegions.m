ea4a005120e33178d8a7ed01d92deda9ca72ac8b
mh105 <alexhe0218@gmail.com>
2025-01-16 16:23:50 -0800

    STY: remove unnecessary find() and clean up indentations

--- toolbox/TFpeak_functions/mergeRegions.m
+++ toolbox/TFpeak_functions/mergeRegions.m
@@ -1,39 +1,39 @@
 function [regions, borders, adj_mat, pick_update] = mergeRegions(regions,a,b,lbls,borders,adj_mat)
-%MERGEREGIONS updates the pixel lists in region and borders and the adjacencies in 
-% adj_mat according to mergion regions b into region a. It also returns an 
+%MERGEREGIONS updates the pixel lists in region and borders and the adjacencies in
+% adj_mat according to mergion regions b into region a. It also returns an
 % indicator for what edge weights need to be updated
 %
-% Usage: 
+% Usage:
 %   [regions, borders, adj_mat, pick_update] = mergeRegions(regions,a,b,lbls,borders,adj_mat)
 %
-% INPUTS: 
-%   regions   -- a 1D cell array with each cell containing a vector of linear 
+% INPUTS:
+%   regions   -- a 1D cell array with each cell containing a vector of linear
 %            indices of the pixels in the region.
 %   a     -- region label to be merged into
 %   b     -- vector of region labels to be merged into a
 %   lbls  -- a vector, of same dimension as regions, with labels for
 %            corresponding regions.
 %   borders -- a 1D cell array, of same size as regions, with each cell
-%            containing the vector of linear indices of the boundary 
+%            containing the vector of linear indices of the boundary
 %            pixels of the corresponding region.
-%   adj_mat -- a three-column matrix of directed region adjacency weights. 
+%   adj_mat -- a three-column matrix of directed region adjacency weights.
 %            each row contains region lables of two adjacent regions.
 %            the first column are "to regions", the second column
 %            are "from regions, and the third column is the weight.
 %
 % OUTPUTS:
-%   regions, borders, adj_mat -- versions of inputs after merger 
+%   regions, borders, adj_mat -- versions of inputs after merger
 %   pick_update        -- logical vector indicating weights that need to be
 %                         updated
 %
 %   Copyright 2024 Prerau Lab - http://www.sleepEEG.org
 %   This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 %   (http://creativecommons.org/licenses/by-nc-sa/4.0/)
-%      
+%
 %   Please provide the following citation for all use:
-%       Patrick A Stokes, Preetish Rath, Thomas Possidente, Mingjian He, Shaun Purcell, Dara S Manoach, 
-%       Robert Stickgold, Michael J Prerau, Transient Oscillation Dynamics During Sleep Provide a Robust Basis 
-%       for Electroencephalographic Phenotyping and Biomarker Identification, 
+%       Patrick A Stokes, Preetish Rath, Thomas Possidente, Mingjian He, Shaun Purcell, Dara S Manoach,
+%       Robert Stickgold, Michael J Prerau, Transient Oscillation Dynamics During Sleep Provide a Robust Basis
+%       for Electroencephalographic Phenotyping and Biomarker Identification,
 %       Sleep, 2022;, zsac223, https://doi.org/10.1093/sleep/zsac223
 %
 %**********************************************************************
@@ -47,7 +47,7 @@ if nargin < 5
 end
 
 %Find the corresponding label for region a
-a_lbl_idx = find(lbls==a);        
+a_lbl_idx = lbls==a;
 if ~isempty(borders)
     border_a = borders{a_lbl_idx};
 end
@@ -59,11 +59,11 @@ for ii = 1:length(b)
 
     %Get the pixels in region b
     region_b_lidx = regions{b_lbl_idx};
-    
+
     %Update a to include all the b pixels
     regions{a_lbl_idx} = unique([regions{a_lbl_idx}; region_b_lidx]);
     regions{b_lbl_idx} = [];
-    
+
     %Update the borders
     if ~isempty(borders)
         border_b = borders{b_lbl_idx};
@@ -71,14 +71,14 @@ for ii = 1:length(b)
         borders{a_lbl_idx} = border_a;
         borders{b_lbl_idx} = [];
     end
-    
+
     %Update the adjacency matrix
     if ~isempty(adj_mat)
         %Connect b's neighbors to a
         cnx_b1 = adj_mat(:,1)==b(ii);
         cnx_b2 = adj_mat(:,2)==b(ii);
         adj_mat(cnx_b1,1) = a;
-        adj_mat(cnx_b2,2) = a;       
+        adj_mat(cnx_b2,2) = a;
 
         % Look for regions encircled by the merge
         nbrs = [adj_mat(cnx_b1,2); adj_mat(cnx_b2,1)];
@@ -89,7 +89,7 @@ for ii = 1:length(b)
             cnx2 = adj_mat(:,2)==nbrs(jj);
 
             %Merge the encircled region with a
-            if ~any(a~=adj_mat(cnx1,2)) && ~any(a~=adj_mat(cnx2,1))   
+            if ~any(a~=adj_mat(cnx1,2)) && ~any(a~=adj_mat(cnx2,1))
 
                 ii_lbl_idx = lbls==nbrs(jj);
                 region_ii_lidx = regions{ii_lbl_idx};
@@ -99,11 +99,11 @@ for ii = 1:length(b)
                 border_a = setxor(border_a,border_ii);
                 borders{a_lbl_idx} = border_a;
                 borders{ii_lbl_idx} = [];
-                
+
                 adj_mat(cnx1,1) = a;
                 adj_mat(cnx2,2) = a;
-            end 
-        end 
+            end
+        end
     end
 end
 
