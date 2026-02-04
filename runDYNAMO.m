03c07f9974135449ed7bb0e9675c62511893c1f5
mh105 <alexhe0218@gmail.com>
2026-02-04 11:37:18 -0800

    DOC: add comments about mode parameterization

--- runDYNAMO.m
+++ runDYNAMO.m
@@ -285,7 +285,7 @@ end
 
 function [SOPH_paramfit] = createSOPHparamfitStruct(params, fitobj, gof, model_SOPH, wshed_img)
 SOPH_paramfit = struct;
-SOPH_paramfit.params = params;
+SOPH_paramfit.params = params; % Columns are: [amp0, fmean0, fstd0, pmean0, pstd0, theta0]
 SOPH_paramfit.fitobj = fitobj;
 SOPH_paramfit.gof = gof;
 SOPH_paramfit.model_SOPH = model_SOPH;
