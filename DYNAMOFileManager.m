5b3e1fe311ec5e342eb4c8f3fad86870e3a674f5
mh105 <alexhe0218@gmail.com>
2026-02-18 12:32:15 -0800

    BF: fix a bug with resetAll() button

--- DYNAMOFileManager.m
+++ DYNAMOFileManager.m
@@ -1267,7 +1267,7 @@ classdef DYNAMOFileManager < matlab.apps.AppBase & DYNAMO
         end
         
         function createDYNAMOSettingsTab(app)
-            app.DYNAMOOptionsApp(false, app.UIFigure, app.DYNAMOSettingsTab,false);
+            app.DYNAMOOptionsApp(false, app.UIFigure, app.DYNAMOSettingsTab, false);
         end
         
         %% ================== BUTTON CALLBACKS ==================
