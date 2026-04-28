b0a8f7e4c847f9e105099f594fa7dca8245e54b7
Mingjian He <49259768+mh105@users.noreply.github.com>
2026-04-28 09:19:09 -0700

    doc: avoid new lines for specifying backend

--- toolbox/TFpeak_functions/computeTFPeaks.m
+++ toolbox/TFpeak_functions/computeTFPeaks.m
@@ -329,12 +329,10 @@ if double_watershed
     % can use mask_spectrogram_mex. When backend='matlab', regions/borders
     % from the MATLAB path feed the pure-MATLAB maskSpectrogram.
     [stats_table, regions, borders, labels_img] = runSegmentedData(spect, stimes, sfreqs, baseline, seg_time, downsample_spect, compute_features, ...
-        dur_min, bw_min, merge_thresh, max_merges, trim_vol, verbose-1 + double(debug_mode), show_pbar, debug_mode, ...
-        'backend', backend);
+        dur_min, bw_min, merge_thresh, max_merges, trim_vol, verbose-1 + double(debug_mode), show_pbar, debug_mode, 'backend', backend);
 else
     stats_table = runSegmentedData(spect, stimes, sfreqs, baseline, seg_time, downsample_spect, compute_features, ...
-        dur_min, bw_min, merge_thresh, max_merges, trim_vol, verbose-1 + double(debug_mode), show_pbar, debug_mode, ...
-        'backend', backend);
+        dur_min, bw_min, merge_thresh, max_merges, trim_vol, verbose-1 + double(debug_mode), show_pbar, debug_mode, 'backend', backend);
 end
 
 tfp_timings.extract_pass1 = toc(tfp);
@@ -397,8 +395,7 @@ if double_watershed
     tfp = tic;
 
     stats_table = runSegmentedData(spect_masked, stimes, sfreqs, baseline, seg_time, downsample_spect, compute_features, ...
-        dur_min, bw_min, merge_thresh, max_merges, trim_vol, verbose-1 + double(debug_mode), show_pbar, debug_mode, ...
-        'backend', backend);
+        dur_min, bw_min, merge_thresh, max_merges, trim_vol, verbose-1 + double(debug_mode), show_pbar, debug_mode, 'backend', backend);
 
     tfp_timings.extract_pass2 = toc(tfp);
     if verbose
